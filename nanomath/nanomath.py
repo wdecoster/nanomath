443c64db39b068c81b5c8d2fa2b0eed8411cb11d
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-06-21 18:51:43 +0200

    moved function to write out stats from nanoplot to here

--- nanomath/nanomath.py
+++ nanomath/nanomath.py
@@ -41,11 +41,37 @@ def readstats(readlengths, qualities):
 	res["MedianLength"] = np.median(readlengths)
 	res["MeanLength"] = np.mean(readlengths)
 	indices_top_L = np.argpartition(readlengths, -5)[-5:]
-	res["MaxLengthsAndQ"] = [zip(list(readlengths[indices_top]),  list(qualities[indices_top]))]
+	res["MaxLengthsAndQ"] = [list(e) for e in zip(list(readlengths[indices_top_L]),  list(qualities[indices_top_L]))]
 	indices_top_Q = np.argpartition(qualities, -5)[-5:]
-	res["MaxQualsAndL"] = [zip(list(readlengths[indices_top]),  list(qualities[indices_top]))]
-	qualgroups = [q for q in range(5,50,5) if q < res["MaxQual"] + 5]
+	res["MaxQualsAndL"] = [list(e) for e in zip(list(readlengths[indices_top_Q]),  list(qualities[indices_top_Q]))]
+	qualgroups = [q for q in range(5,50,5) if q < np.amax(qualities) + 5]
 	res["QualGroups"] = dict()
 	for q in qualgroups:
-		numberAboveQ = np.sum(readlengths > q)
+		numberAboveQ = np.sum(qualities > q)
 		res["QualGroups"][q] = (numberAboveQ, numberAboveQ / res["NumberOfReads"])
+	return res
+
+
+def writeStats(datadf, outputfile):
+	'''
+	Call calculation function and write to file
+	'''
+	stat = readstats(np.array(datadf["lengths"]), np.array(datadf["quals"]))
+	with open(outputfile, 'wt') as output:
+		output.write("Number of reads:\t{}\n".format(stat["NumberOfReads"]))
+		output.write("Total bases:\t{}\n".format(stat["TotalBases"]))
+		output.write("Median read length:\t{}\n".format(stat["MedianLength"]))
+		output.write("Mean read length:\t{}\n".format(round(stat["MeanLength"],2)))
+		output.write("Readlength N50:\t{}\n".format(getN50(datadf["lengths"])))
+		output.write("\n")
+		output.write("Top 5 read lengths and their average basecall quality score:\n")
+		for length, qual in sorted(stat["MaxLengthsAndQ"], key=lambda x: x[0], reverse=True):
+			output.write("Length: {}bp\tQ: {}\n".format(length, round(qual, 2)))
+		output.write("\n")
+		output.write("Top 5 average basecall quality scores and their read lengths:\n")
+		for length, qual in sorted(stat["MaxQualsAndL"], key=lambda x: x[1], reverse=True):
+			output.write("Length: {}bp\tQ: {}\n".format(length, round(qual, 2)))
+		output.write("\n")
+		output.write("Number of reads and fraction above quality cutoffs:\n")
+		for q in sorted(stat["QualGroups"].keys()):
+			output.write("Q{}:\t{}\t{}%\n".format(q, stat["QualGroups"][q][0], round(100*stat["QualGroups"][q][1],2)))
